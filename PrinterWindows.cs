d1147175b5b84964059785ea691a57c382656176
Taqi <mtaqiyuddin101@gmail.com>
2022-06-10 11:15:09 +0700

    Polymorphism

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class PrinterWindows
+    {
+        public string Name { get; set; }
+        public virtual void Show()
+        {
+
+        }
+        public virtual void Print()
+        {
+
+        }
+    }
+}
